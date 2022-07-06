75cafb0d9f93f39f5adedf04b127a36099d7aa61
sajid-munawar <sajidmunawar666@gmail.com>
2022-07-07 00:49:12 +0500

    Create class component

---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (<div>h11</div>);
+  }
+}
+export default Calculator;
