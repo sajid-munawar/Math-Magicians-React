303dd3c21294b6228c4ba681e7c44cb51c92017f
sajid-munawar <sajidmunawar666@gmail.com>
2022-07-08 15:46:12 +0500

    Add buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,35 +1,41 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState((state) => calculate(state, event.target.textContent));
   }
 
   render() {
     return (
       <div>
         <ul className="container">
-          <li className="item1">0</li>
-          <li className="item2">AC</li>
-          <li className="item3">+/-</li>
-          <li className="item4">%</li>
-          <li className="item5 y-bg">/</li>
-          <li className="item6">7</li>
-          <li className="item7">8</li>
-          <li className="item8">9</li>
-          <li className="item9 y-bg">x</li>
-          <li className="item10">4</li>
-          <li className="item11">5</li>
-          <li className="item12">6</li>
-          <li className="item13 y-bg">-</li>
-          <li className="item14">1</li>
-          <li className="item15">2</li>
-          <li className="item16">3</li>
-          <li className="item17 y-bg">+</li>
-          <li className="item18">0</li>
-          <li className="item19">.</li>
-          <li className="item20 y-bg">=</li>
+          <li className="item1"><button type="button" onClick={this.handleClick}>0</button></li>
+          <li className="item2"><button type="button" onClick={this.handleClick}>AC</button></li>
+          <li className="item3"><button type="button" onClick={this.handleClick}>+/-</button></li>
+          <li className="item4"><button type="button" onClick={this.handleClick}>%</button></li>
+          <li className="item5 y-bg"><button type="button" onClick={this.handleClick}>÷</button></li>
+          <li className="item6"><button type="button" onClick={this.handleClick}>7</button></li>
+          <li className="item7"><button type="button" onClick={this.handleClick}>8</button></li>
+          <li className="item8"><button type="button" onClick={this.handleClick}>9</button></li>
+          <li className="item9 y-bg"><button type="button" onClick={this.handleClick}>x</button></li>
+          <li className="item10"><button type="button" onClick={this.handleClick}>4</button></li>
+          <li className="item11"><button type="button" onClick={this.handleClick}>5</button></li>
+          <li className="item12"><button type="button" onClick={this.handleClick}>6</button></li>
+          <li className="item13 y-bg"><button type="button" onClick={this.handleClick}>-</button></li>
+          <li className="item14"><button type="button" onClick={this.handleClick}>1</button></li>
+          <li className="item15"><button type="button" onClick={this.handleClick}>2</button></li>
+          <li className="item16"><button type="button" onClick={this.handleClick}>3</button></li>
+          <li className="item17 y-bg"><button type="button" onClick={this.handleClick}>+</button></li>
+          <li className="item18"><button type="button" onClick={this.handleClick}>0</button></li>
+          <li className="item19"><button type="button" onClick={this.handleClick}>.</button></li>
+          <li className="item20 y-bg"><button type="button" onClick={this.handleClick}>=</button></li>
         </ul>
       </div>
     );
