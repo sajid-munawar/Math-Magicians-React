ac81f668cf10e93146d4a723d71e2f67194a6ad1
sajid-munawar <sajidmunawar666@gmail.com>
2022-07-15 10:53:40 +0500

    Remove unused code, comments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
-// import { Link } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-// import Calculator from './components/Calculator';
 
 function App() {
   return (
